8ac48977cf71585ff98ae7c044422d14f6ff2536
Vincent Tam <vtam2012@gmail.com>
2018-09-11 21:43:45 -0700

    deploy

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = ({name, entries}) => {
+  return (
+    <div>
+      <div className='whilte f3'>
+        {`${name}, your current entry count is ...`}
+      </div>
+      <div className='whilte f1'>
+        {entries}
+      </div>
+    </div>
+  );
+}
+
+export default Rank;
